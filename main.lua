f1873458a27fcb53905215d767359a51cb41468a
V. <bartosz.bentkowski@gmail.com>
2014-01-04 13:05:54 +0100

    use externals

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
-local event = CreateFrame("Frame")
-event:SetScript("OnEvent", function(self, event, ...) self[event](self, event, ...) end)
-event:RegisterEvent"ZONE_CHANGED_NEW_AREA"
+local addon, ns = ...
 
-event.ZONE_CHANGED_NEW_AREA = function(self, event, ...)
+ns.RegisterEvent("ZONE_CHANGED_NEW_AREA", function()
 	local newtime = time();
 	local elapsed = difftime(newtime,lastcycle);
 	if elapsed > 300 then
 		collectgarbage();
 		lastcycle = newtime;
 	end
-end
+end)
